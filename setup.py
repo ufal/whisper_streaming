06224baa6c8eb02d62b55d215442ce07a680752a
Daniel Nurkowski <danieln@speechmatics.com>
2025-01-23 15:42:52 +0000

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="whisper_streaming",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where="whisper_streaming"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
