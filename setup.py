99bc9523d525120ab232ac9a7afa2bff7d55c916
Daniel Nurkowski <danieln@speechmatics.com>
2025-01-23 15:48:25 +0000

    add __init__file

--- setup.py
+++ setup.py
@@ -2,8 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="whisper_streaming",
-    version="0.1",
+    version="0.2.1",
     packages=find_packages(where="whisper_streaming"),
+    include_package_data=True,
+    py_modules=[
+      "whisper_streaming.whisper_online",
+      "whisper_streaming.whisper_online_server",
+      "whisper_streaming.silero_vad_iterator",
+      "whisper_streaming.__init__",
+    ],
+    license="MIT",
+    author="Some Author",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
