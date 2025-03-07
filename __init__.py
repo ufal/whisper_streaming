2b701d65362d8c8f295fad55a8b5d9b68ea372c1
evandrocoan <evandrocoan@hotmail.com>
2025-03-07 16:20:11 -0300

    Expose VACOnlineASRProcessor on __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 from .whisper_online import OnlineASRProcessor
+from .whisper_online import VACOnlineASRProcessor
 from .whisper_online import OpenaiApiASR
 from .whisper_online import create_tokenizer
